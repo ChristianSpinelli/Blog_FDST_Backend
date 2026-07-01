0515ef24f973f57f8f429d262f69549b20e50cce
c.spin <c.spinelli93@gmail.com>
2026-07-01 15:20:18 -0300

    criado middleware de autenticação para verificar permissões

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+
+export interface AuthenticatedRequest extends Request {
+  user?: {
+    id: number;
+    role: string;
+  };
+}
+
+export const authorizeRoles = (...allowedRoles: string[]) => {
+  return (req: AuthenticatedRequest, res: Response, next: NextFunction): void => {
+
+    //verifica se está autenticado
+    if (!req.user) {
+      res.status(401).json({ message: 'Não autenticado.' });
+      return;
+    }
+    
+    //verifica se inclue a rola específica para acessar o serviço correspondente.
+    if (!allowedRoles.includes(req.user.role)) {
+      res.status(403).json({ message: 'Acesso negado. Permissão insuficiente.' });
+      return;
+    }
+
+    next();
+  };
+};
