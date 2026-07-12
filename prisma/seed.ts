7eefdfcd05e4300814abb6536c51b50aa0aa6c19
c.spin <c.spinelli93@gmail.com>
2026-07-12 20:28:28 -0300

    seed: falhou a criação da seed reajuste no código para gerar o admin corretamente

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = "postgresql://c.spin:blog123@db:5432/blog?schema=public";
+}
 const prisma = new PrismaClient();
 
 async function main() {
